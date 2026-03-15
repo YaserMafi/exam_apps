eaa55b7cc974025b93b8cc256410d53647ae43aa
YaserMafi <113743920+YaserMafi@users.noreply.github.com>
2026-03-15 09:20:42 +0430

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import datetime
 
 app = Flask(__name__)
 
+# صفحه اصلی (فرم)
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
+# ثبت جواب‌ها
 @app.route("/submit", methods=["POST"])
 def submit():
 
@@ -17,17 +19,22 @@ def submit():
 
     time = datetime.datetime.now()
 
-    try:
-        with open("answers.txt", "a", encoding="utf-8") as f:
-            f.write(f"Name: {name}\n")
-            f.write(f"Age: {age}\n")
-            f.write(f"Answer: {q1}\n")
-            f.write(f"Time: {time}\n")
-            f.write("--------------------\n")
-    except:
-        pass
+    with open("answers.txt", "a", encoding="utf-8") as f:
+        f.write(f"Name: {name}\n")
+        f.write(f"Age: {age}\n")
+        f.write(f"Answer: {q1}\n")
+        f.write(f"Time: {time}\n")
+        f.write("----------------------\n")
 
-    return render_template("index.html")
+    return "جواب ثبت شد"
+
+
+# دیدن جواب‌ها
+@app.route("/answers")
+def show_answers():
+    with open("answers.txt","r",encoding="utf-8") as f:
+        data = f.read()
+    return "<pre>" + data + "</pre>"
 
 
 if __name__ == "main":
