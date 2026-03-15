56f40de62113bee02eda4ad5a4333742972e157a
YaserMafi <113743920+YaserMafi@users.noreply.github.com>
2026-03-15 08:56:51 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 from flask import Flask, render_template, request
 import datetime
 
-app = Flask(__name__)
+app = Flask(name)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/submit', methods=['POST'])
+
+@app.route("/submit", methods=["POST"])
 def submit():
 
-    name = request.form['name']
-    age = request.form['age']
-    q1 = request.form['q1']
+    name = request.form.get("name")
+    age = request.form.get("age")
+    q1 = request.form.get("q1")
 
     time = datetime.datetime.now()
 
-    with open("answers.txt","a",encoding="utf-8") as f:
-        f.write(f"Name: {name}\n")
-        f.write(f"Age: {age}\n")
-        f.write(f"Answer: {q1}\n")
-        f.write(f"Time: {time}\n")
-        f.write("-----------------\n")
+    try:
+        with open("answers.txt", "a", encoding="utf-8") as f:
+            f.write(f"Name: {name}\n")
+            f.write(f"Age: {age}\n")
+            f.write(f"Answer: {q1}\n")
+            f.write(f"Time: {time}\n")
+            f.write("--------------------\n")
+    except:
+        pass
 
-    return "جواب ثبت شد"
+    return render_template("index.html")
 
-if __name__ == "main":
+
+if name == "main":
     app.run(host="0.0.0.0", port=5000)
